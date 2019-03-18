26b808fc2877826f9c84ebf3f5dd4cd3fb005943
Ryan1700 <tytanium1414@gmail.com>
2019-03-18 09:29:35 -0300

    Limite de movimentaçao

--- Assets/Scripts/Simplemove.cs
+++ Assets/Scripts/Simplemove.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class Simplemove : MonoBehaviour
 {
     [SerializeField]
-private float SpeedFactor = 1f;
+    private float SpeedFactor = 1f;
+    [SerializeField]
+    private float LimitTop = 4f;
+    [SerializeField]
+    private float LimitBottom = -4f;
+    [SerializeField]
+    private float LimitLeft = -8f;
+    [SerializeField]
+    private float LimitRight = 8f;
+
     void Start()
     {
         
@@ -22,5 +31,17 @@ private float SpeedFactor = 1f;
        
         transform.position = transform.position + velocity * Time.deltaTime;
         
+        if(transform.position.y < LimitBottom){
+            transform.position = new Vector3(transform.position.x, LimitBottom, transform.position.z);
+        }
+        if(transform.position.x > LimitTop){
+            transform.position = new Vector3(transform.position.x, LimitTop, transform.position.z);
+        }
+        if(transform.position.x < LimitLeft){
+            transform.position = new Vector3(LimitLeft, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > LimitRight){
+            transform.position = new Vector3(LimitRight, transform.position.y, transform.position.z);
+        }
     }
 }
