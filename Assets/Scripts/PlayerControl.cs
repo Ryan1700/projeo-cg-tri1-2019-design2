da0380464094adcf6e84f429fbdece8bd005dfb0
Ryan1700 <tytanium1414@gmail.com>
2019-05-21 21:33:53 -0300

    Contando moedas

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Coin"))
-        {
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Coin")) {
             SFXManager.instance.ShowCoinParticles(other.gameObject);
             AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
             Destroy(other.gameObject);
+            SceneManager.instance.IncrementCoinCount();
         }
     }
 }
