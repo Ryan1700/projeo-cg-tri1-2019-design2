fdfb7474901ce7041351e45fb31dd0478ff91a70
Ryan1700 <tytanium1414@gmail.com>
2019-05-21 21:13:34 -0300

    Criando a classe UIComponents e fazendo as referencias

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class SceneManager : MonoBehaviour
 {
     [SerializeField]
-
-    private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f);
+    private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f); 
     [SerializeField]
-
-
     private Transform Tape = null;
+
+    public UIComponents uiComponents;
+
+    void Awake() {
+        Assert.IsNotNull(Tape);
+    }
 
     void Start()
     {
@@ -21,6 +25,5 @@ public class SceneManager : MonoBehaviour
     void Update()
     {
         Tape.position = Tape.position + TapeSpeed * Time.deltaTime;
-        
     }
 }
