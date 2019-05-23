bd82a2a8d0d1d2ef632142e41c7c922094f8b361
Ryan1700 <tytanium1414@gmail.com>
2019-05-23 11:46:02 -0300

    Trocando SceneManager por LevelManager e apagando LifeCount

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class SceneManager : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
-    public static SceneManager instance = null;
+    public static LevelManager instance = null;
 
     [SerializeField]
-    private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f); 
+    private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f);
     [SerializeField]
     private Transform Tape = null;
 
@@ -16,24 +16,28 @@ public class SceneManager : MonoBehaviour
 
     SceneData sceneData = new SceneData();
 
-    void Awake() {
+    void Awake()
+    {
         Assert.IsNotNull(Tape);
-        if (instance == null) {
+        if (instance == null)
+        {
             instance = this;
         }
     }
-    
+
     void Update()
     {
         Tape.position = Tape.position + TapeSpeed * Time.deltaTime;
         DisplayHudData();
     }
 
-    public void IncrementCoinCount() {
+    public void IncrementCoinCount()
+    {
         sceneData.coinCount++;
     }
 
-    void DisplayHudData() {
+    void DisplayHudData()
+    {
         uiComponents.hud.txtCoinCount.text = "x " + sceneData.coinCount;
     }
 }
