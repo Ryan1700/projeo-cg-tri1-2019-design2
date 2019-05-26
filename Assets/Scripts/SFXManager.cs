eac0d053ef99fc1d2d926b8289468bd390a736da
Ryan1700 <tytanium1414@gmail.com>
2019-05-26 11:36:11 -0300

    Particulas quando o player morre

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -7,19 +7,23 @@ public class SFXManager : MonoBehaviour
     public static SFXManager instance = null;
 
     public GameObject coinParticles;
+    public GameObject dieParticles;
 
-    void Awake()
-    {
-        if (instance == null)
-        {
+    void Awake() {
+        if (instance == null) {
             instance = this;
         }
     }
 
-    public void ShowCoinParticles(GameObject obj)
-    {
+    public void ShowCoinParticles(GameObject obj) {
         GameObject particles = Instantiate(coinParticles, obj.transform.position, Quaternion.identity);
         GameObject tape = GameObject.Find("Tape");
         particles.transform.SetParent(tape.transform);
-    }
+    } 
+
+    public void ShowDieParticles(GameObject obj) {
+        GameObject particles = Instantiate(dieParticles, obj.transform.position, Quaternion.identity);
+        GameObject tape = GameObject.Find("Tape");
+        particles.transform.SetParent(tape.transform);
+    } 
 }
